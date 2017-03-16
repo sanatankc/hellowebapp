af8b3b48e41432fb7188c323e5573a94bb01c16e
Sanatan kumar <sanatankumarchaudhary@gmail.com>
2017-03-16 20:30:11 +0530

    Chapter 6 Completed

--- hellowebapp/collection/admin.py
+++ hellowebapp/collection/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Thing
 
+# set up automated slugcreation
+class ThingAdmin(admin.ModelAdmin):
+    model = Thing
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
 # Register your models here.
+admin.site.register(Thing, ThingAdmin)
--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request): 
     # this is your new view
-    return render(request, 'index.html')
+    number = 1
+    return render(request, 'index.html', {
+        'number': number,
+        })
