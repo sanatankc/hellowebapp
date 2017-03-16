2f01d68c72f6eb29afa8a70754bc3ba4cfdaeba6
Sanatan kumar <sanatankumarchaudhary@gmail.com>
2017-03-16 06:00:59 +0530

    chapter 4 compeleted

--- hellowebapp/hellowebapp/settings.py
+++ hellowebapp/hellowebapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'collection',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'hellowebapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'collection/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
